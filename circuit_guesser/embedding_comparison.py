ddfa798de883b8667b9e2eb87a7701069caa02a2
dupontm <martin.dupont-ext@rio.cloud>
2020-01-26 19:03:54 +0100

    * Added timing analysis to embedding_comparison.py

--- circuit_guesser/embedding_comparison.py
+++ circuit_guesser/embedding_comparison.py
@@ -5,6 +5,7 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 import circuits as c
+import time
 
 # ============================================================== #
 max_layers = 7
@@ -16,6 +17,7 @@ sampler = samplers.MockSampler([])
 
 output = np.zeros((max_layers - 1, max_layers + 1))
 n_vars_array = np.zeros((max_layers - 1, max_layers + 1))
+times_array = np.zeros((max_layers - 1, max_layers + 1))
 
 dirname = os.path.dirname(__file__) + "/embedding_results"
 embeddings_file = dirname + "/embeddings.pickle"
@@ -26,17 +28,23 @@ variables_file = dirname + "/num_variables.pickle"
 if os.path.exists(variables_file):
     os.remove(variables_file)
 
+times_file = dirname + "/times.pickle"
+if os.path.exists(times_file):
+    os.remove(times_file)
+
 for n_layers in range(1, max_layers):
     n_s, _ = c.get_ns_nx(n_layers)
-    weights = [1 for i in range(n_s)]
     for batch in range(0, n_layers + 2): # with batch, I want to take actual batch sizes of size 2 ** batch. there is N_layers +1 x vars which means range(n_layers + 2 hits that)
         n_batches = 2 ** (n_layers + 1 - batch )
-        strategy = strat.SmarterStrategy(n_layers, n_embedding_tries, 100, weights, sampler, n_batches)
+        strategy = strat.SmarterStrategy(n_layers, n_embedding_tries, 100, sampler, n_batches)
         try:
             print("===========================================================")
             print("n_layers : {}, batch_size: {}".format(n_layers, 2 ** batch))
+            start = time.time()
             embedding = strategy.make_embedding()
+            end = time.time()
             worst_chain_length = max(len(value) for value in embedding.values())
+            embedding_time = end - start
         except Exception as e: # errors are not exceptions!?!?!?!??!?
             print(e)
             worst_chain_length = np.inf
@@ -49,6 +57,9 @@ for n_layers in range(1, max_layers):
 
         n_vars_array[n_layers - 1, batch] = n_vars
         pickle.dump(n_vars_array, open(variables_file, "wb"))
+
+        times_array[n_layers - 1, batch] = embedding_time
+        pickle.dump(times_array, open(times_file, "wb"))
 
 plt.imshow(output)
 plt.colorbar()
@@ -70,6 +81,17 @@ plt.yticks(range(max_layers - 1), range(1, max_layers + 1))
 plt.xlabel('batch size')
 plt.xticks(range(max_layers + 1), [ 2 ** i for i in range(max_layers + 1)])
 plt.savefig(dirname + '/num_variables.png')
+plt.show()
+plt.close()
+
+plt.imshow(times_array)
+plt.colorbar()
+plt.title('time to find embedding')
+plt.ylabel('time [s]')
+plt.yticks(range(max_layers - 1), range(1, max_layers + 1))
+plt.xlabel('batch size')
+plt.xticks(range(max_layers + 1), [ 2 ** i for i in range(max_layers + 1)])
+plt.savefig(dirname + '/times.png')
 plt.show()
 plt.close()
 
--- circuit_guesser/plot_embeddings.py
+++ circuit_guesser/plot_embeddings.py
@@ -6,6 +6,7 @@ dirname = os.path.dirname(__file__) + "/embedding_results"
 
 output = pickle.load(open('{}/embeddings.pickle'.format(dirname), "rb" ))
 n_vars_array = pickle.load(open('{}/num_variables.pickle'.format(dirname), "rb" ))
+times_array = pickle.load(open('{}/times.pickle'.format(dirname), "rb" ))
 
 n_layers = len(output[:, 0])
 
@@ -30,5 +31,16 @@ plt.yticks(range(n_layers - 1), range(1, n_layers + 1))
 plt.xlabel('batch size')
 plt.xticks(range(n_layers + 1), [ 2 ** i for i in range(n_layers + 1)])
 plt.savefig(dirname + '/num_variables.png')
+plt.show()
+plt.close()
+
+plt.imshow(times_array)
+plt.colorbar()
+plt.title('time to find embedding')
+plt.ylabel('time [s]')
+plt.yticks(range(n_layers - 1), range(1, n_layers + 1))
+plt.xlabel('batch size')
+plt.xticks(range(n_layers + 1), [ 2 ** i for i in range(n_layers + 1)])
+plt.savefig(dirname + '/times.png')
 plt.show()
 plt.close()
