54b4c143bc6763a83f3d92e43ad33bece70c9c9c
dupontm <martin.dupont-ext@rio.cloud>
2020-02-01 22:11:01 +0100

    * Fixed naming of output files

--- circuit_guesser/battle_royale_qbsolv.py
+++ circuit_guesser/battle_royale_qbsolv.py
@@ -40,7 +40,7 @@ def solve_with_strategy(sampler, n_tries, x_data, y_data, weights, record_type, 
 
 
 # ============================================================== #
-max_layers = 4
+max_layers = 7
 n_problems_per_size = 10
 n_tries = 20
 use_real_dwave = False
@@ -56,7 +56,7 @@ n_vars_array = np.zeros((max_layers - 1, max_layers + 1))
 dirname = os.path.dirname(__file__) + "/battle_results_qbsolv"
 now = datetime.datetime.now()
 date_string = now.strftime("%Y_%m_%d_%H:%M")
-records_file = dirname + "/records.pickle".format(date_string)
+records_file = dirname + "/records_{}.pickle".format(date_string)
 if os.path.exists(records_file):
     os.remove(records_file)
 
